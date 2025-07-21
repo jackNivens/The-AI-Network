d6f69e79c421f9d4250010d85760619688308446
jackNivens <contact.jnivens@gmail.com>
2025-07-21 12:24:10 +0100

    Delete Pages directory

--- Pages/Index.js
+++
@@ -1,8 +0,0 @@
-export default function Home() {
-  return (
-    <div style={{ textAlign: "center", padding: "50px" }}>
-      <h1 style={{ fontSize: "40px", fontWeight: "bold" }}>The AI Network</h1>
-      <p>Welcome to your automated AI tools directory!</p>
-    </div>
-  );
-}
